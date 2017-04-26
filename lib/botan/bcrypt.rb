9b370d7d57a5d21d6438619ae0d5b0dddec3e56d
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2017-04-26 19:44:01 -0400

    Make bcrypt a little easier to use/safer with keyword args.

--- lib/botan/bcrypt.rb
+++ lib/botan/bcrypt.rb
@@ -1,5 +1,5 @@
 module Botan
-  def self.bcrypt(passwd, rng, work_factor=10)
+  def self.bcrypt(password, work_factor: 10, rng: Botan::RNG.new)
     out_len = 64
     out_buf = FFI::MemoryPointer.new(:uint8, out_len)
     flags = 0
@@ -7,14 +7,14 @@ module Botan
     out_len_ptr.write(:size_t, out_len)
     Botan.call_ffi(:botan_bcrypt_generate,
                    out_buf, out_len_ptr,
-                   passwd, rng.ptr, work_factor, flags)
+                   password, rng.ptr, work_factor, flags)
     result = out_buf.read_bytes(out_len_ptr.read(:size_t))
     result = result[0..-2] if result[-1] == "\x00"
     result
   end
 
-  def self.bcrypt_valid?(passwd, bcrypt)
-    rc = LibBotan.botan_bcrypt_is_valid(passwd, bcrypt)
+  def self.bcrypt_valid?(password:, phash:)
+    rc = LibBotan.botan_bcrypt_is_valid(password, phash)
     return rc == 0
   end
 end # module
--- spec/bcrypt_spec.rb
+++ spec/bcrypt_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Botan.method(:bcrypt) do
-  let(:phash) { Botan.bcrypt('password', Botan::RNG.new) }
+  let(:phash) { Botan.bcrypt('password') }
 
   it 'creates hashes with the expected length' do
     expect(phash.bytesize).to eql 60
@@ -10,15 +10,24 @@ end
 
 describe Botan.method(:bcrypt_valid?) do
   let(:password) { 'password' }
-  let(:phash) { Botan.bcrypt(password, Botan::RNG.new) }
+  let(:phash) { Botan.bcrypt(password, work_factor: 5, rng: Botan::RNG.new) }
 
   it 'accepts correct passwords' do
-    expect(Botan.bcrypt_valid?(password, phash)).to eql true
+    expect(
+      Botan.bcrypt_valid?(password: password,
+                          phash: phash)
+    ).to eql true
   end
 
   it 'rejects incorrect passwords' do
-    expect(Botan.bcrypt_valid?('', phash)).to eql false
-    expect(Botan.bcrypt_valid?('wrong', phash)).to eql false
+    expect(
+      Botan.bcrypt_valid?(password: '',
+                          phash: phash)
+    ).to eql false
+    expect(
+      Botan.bcrypt_valid?(password: 'wrong',
+                          phash: phash)
+    ).to eql false
   end
 end
 
