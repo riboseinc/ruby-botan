b6fddd8e89eeeecb568a6a78f1744dd01c09d212
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2017-04-26 19:46:24 -0400

    Use keyword args for pbkdf methods since they take so many parameters. Also return a hash.

--- lib/botan/kdf.rb
+++ lib/botan/kdf.rb
@@ -1,6 +1,6 @@
 module Botan
-  def self.kdf(algo, secret, out_len, salt, label)
-    out_buf = FFI::MemoryPointer.new(:uint8, out_len)
+  def self.kdf(algo:, secret:, key_len:, salt:, label:)
+    out_buf = FFI::MemoryPointer.new(:uint8, key_len)
 
     secret_buf = FFI::MemoryPointer.new(:uint8, secret.bytesize)
     secret_buf.write_bytes(secret)
@@ -16,29 +16,33 @@ module Botan
                    secret_buf, secret_buf.size,
                    salt_buf, salt_buf.size,
                    label_buf, label_buf.size)
-    out_buf.read_bytes(out_len)
+    out_buf.read_bytes(key_len)
   end
 
-  def self.pbkdf(algo, password, out_len, iterations=10000, salt=RNG.new.get(12))
-    out_buf = FFI::MemoryPointer.new(:uint8, out_len)
+  def self.pbkdf(algo:, password:, key_len:, iterations: 10000, salt: RNG.get(12))
+    out_buf = FFI::MemoryPointer.new(:uint8, key_len)
     salt_buf = FFI::MemoryPointer.new(:uint8, salt.bytesize)
     salt_buf.write_bytes(salt)
     Botan.call_ffi(:botan_pbkdf,
-                   algo, out_buf, out_len,
+                   algo, out_buf, key_len,
                    password, salt_buf, salt_buf.size, iterations)
-    return [salt, iterations, out_buf.read_bytes(out_len)]
+    return {salt: salt,
+            iterations: iterations,
+            key: out_buf.read_bytes(key_len)}
   end
 
-  def self.pbkdf_timed(algo, password, out_len, ms_to_run=300, salt=RNG.new.get(12))
-    out_buf = FFI::MemoryPointer.new(:uint8, out_len)
+  def self.pbkdf_timed(algo:, password:, key_len:, ms_to_run: 300, salt: RNG.get(12))
+    out_buf = FFI::MemoryPointer.new(:uint8, key_len)
     salt_buf = FFI::MemoryPointer.new(:uint8, salt.bytesize)
     salt_buf.write_bytes(salt)
     iterations_ptr = FFI::MemoryPointer.new(:size_t)
     Botan.call_ffi(:botan_pbkdf_timed,
-                   algo, out_buf, out_len,
+                   algo, out_buf, key_len,
                    password, salt_buf, salt_buf.size,
                    ms_to_run, iterations_ptr)
-    return [salt, iterations_ptr.read(:size_t), out_buf.read_bytes(out_len)]
+    return {salt: salt,
+            iterations: iterations_ptr.read(:size_t),
+            key: out_buf.read_bytes(key_len)}
   end
 end # module
 
--- spec/kdf_spec.rb
+++ spec/kdf_spec.rb
@@ -3,26 +3,26 @@ require 'spec_helper'
 describe Botan.method(:kdf) do
   it 'produces the expected output' do
     expect(
-      Botan.kdf('KDF2(SHA-1)',
-                Botan.hex_decode('701F3480DFE95F57941F804B1B2413EF'),
-                7,
-                Botan.hex_decode('55A4E9DD5F4CA2EF82'),
-                '')
+      Botan.kdf(algo: 'KDF2(SHA-1)',
+                secret: Botan.hex_decode('701F3480DFE95F57941F804B1B2413EF'),
+                key_len: 7,
+                salt: Botan.hex_decode('55A4E9DD5F4CA2EF82'),
+                label: '')
     ).to eql Botan.hex_decode('fbecb3ccfeec6e')
   end
 end
 
 describe Botan.method(:pbkdf) do
   let(:result) {
-      Botan.pbkdf('PBKDF2(SHA-1)',
-                  '',
-                  32,
-                  10000,
-                  Botan.hex_decode('0001020304050607'))
+      Botan.pbkdf(algo: 'PBKDF2(SHA-1)',
+                  password: '',
+                  key_len: 32,
+                  iterations: 10000,
+                  salt: Botan.hex_decode('0001020304050607'))
   }
-  let(:salt) { result[0] }
-  let(:iterations) { result[1] }
-  let(:psk) { result[2] }
+  let(:salt) { result[:salt] }
+  let(:iterations) { result[:iterations] }
+  let(:key) { result[:key] }
 
   it 'returns the expected salt' do
     expect(salt).to eql "\x00\x01\x02\x03\x04\x05\x06\x07"
@@ -33,20 +33,20 @@ describe Botan.method(:pbkdf) do
   end
 
   it 'produces the expected output' do
-    expect(psk).to eql Botan.hex_decode('59B2B1143B4CB1059EC58D9722FB1C72471E0D85C6F7543BA5228526375B0127')
+    expect(key).to eql Botan.hex_decode('59B2B1143B4CB1059EC58D9722FB1C72471E0D85C6F7543BA5228526375B0127')
   end
 end
 
 describe Botan.method(:pbkdf_timed) do
   let(:result) {
-      Botan.pbkdf_timed('PBKDF2(SHA-256)',
-                        'xyz',
-                        32,
-                        200)
+      Botan.pbkdf_timed(algo: 'PBKDF2(SHA-256)',
+                        password: 'xyz',
+                        key_len: 32,
+                        ms_to_run: 200)
   }
-  let(:salt) { result[0] }
-  let(:iterations) { result[1] }
-  let(:psk) { result[2] }
+  let(:salt) { result[:salt] }
+  let(:iterations) { result[:iterations] }
+  let(:key) { result[:key] }
 
   it 'returns a salt of the expected size' do
     expect(salt.bytesize).to eql 12
@@ -57,17 +57,17 @@ describe Botan.method(:pbkdf_timed) do
   end
 
   it 'produces an output of the correct size' do
-    expect(psk.bytesize).to eql 32
+    expect(key.bytesize).to eql 32
   end
 
   it 'produces the same output with the same inputs (timed and non-timed)' do
     expect(
-      Botan.pbkdf('PBKDF2(SHA-256)',
-                  'xyz',
-                  32,
-                  iterations,
-                  salt)[2]
-    ).to eql psk
+      Botan.pbkdf(algo: 'PBKDF2(SHA-256)',
+                  password: 'xyz',
+                  key_len: 32,
+                  iterations: iterations,
+                  salt: salt)[:key]
+    ).to eql key
   end
 end
 
