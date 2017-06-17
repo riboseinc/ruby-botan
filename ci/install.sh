827415db88ef61b9c7b7bdc897f8a7d713d36bf8
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2017-06-17 08:33:32 -0400

    ci: Use all available cores to build botan.

--- ci/install.sh
+++ ci/install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-set -eu
+set -eux
+
+CORES="2" && [ -r /proc/cpuinfo ] && CORES=$(grep -c '^$' /proc/cpuinfo)
 
 # botan
 if [ ! -e "${BOTAN_INSTALL}/lib/libbotan-2.so" ]; then
   git clone https://github.com/randombit/botan ~/builds/botan
   cd ~/builds/botan
   ./configure.py --prefix="${BOTAN_INSTALL}"
-  make -j2 install
+  make -j${CORES} install
 fi
 
