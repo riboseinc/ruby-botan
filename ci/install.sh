17b790c7fc9111accc84013e6c5e1a3d6a4f4ee3
Daniel Wyatt <Daniel.Wyatt@gmail.com>
2017-09-06 13:25:02 -0400

    ci: Update to match our other configurations.

--- ci/install.sh
+++ ci/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -eux
 
-CORES="2" && [ -r /proc/cpuinfo ] && CORES=$(grep -c '^$' /proc/cpuinfo)
+: "${CORES:=2}"
 
 # botan
-if [ ! -e "${BOTAN_INSTALL}/lib/libbotan-2.so" ]; then
-  git clone https://github.com/randombit/botan ~/builds/botan
-  cd ~/builds/botan
+if [ ! -e "${BOTAN_INSTALL}/lib/libbotan-2.so" ] && [ ! -e "${BOTAN_INSTALL}/lib/libbotan-2.dylib" ]; then
+  git clone https://github.com/randombit/botan "${LOCAL_BUILDS}/botan"
+  cd "${LOCAL_BUILDS}/botan"
   ./configure.py --prefix="${BOTAN_INSTALL}"
   make -j${CORES} install
 fi
