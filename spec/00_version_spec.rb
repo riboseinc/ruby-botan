6b63e17368ed3a981f94a70ac061683c69c29002
Daniel Wyatt <daniel.wyatt@ribose.com>
2018-08-16 21:18:35 -0400

    tests: Show botan library version during test runs.

---
+++ spec/00_version_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+# (c) 2018 Ribose Inc.
+
+require 'spec_helper'
+
+describe LibBotan::LIB_VERSION do
+  puts 'Botan version: ' + LibBotan::LIB_VERSION.join('.')
+end
+
